360cc8e95baea341dfe7ea7d004c3bb203c3fff6
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-17 22:45:18 +0530

    Add @Documents(collection = "rooms") Room.java Err during API testing

--- bytebuzz-backend/src/main/java/com/rachit/bytebuzz/backend/entities/Room.java
+++ bytebuzz-backend/src/main/java/com/rachit/bytebuzz/backend/entities/Room.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.ArrayList;
 import java.util.List;
 
-@Document("rooms")
+@Document(collection = "rooms")
 @Getter
 @Setter
 @NoArgsConstructor
