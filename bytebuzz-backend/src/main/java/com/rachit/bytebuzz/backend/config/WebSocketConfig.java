4e8fc2c5d92225283c292838b39a73cb02ac2e96
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-18 01:27:51 +0530

    feat: Add Config under package WebSocketConfig.java for connection establishing

---
+++ bytebuzz-backend/src/main/java/com/rachit/bytebuzz/backend/config/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package com.rachit.bytebuzz.backend.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
+{
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config)
+    {
+        config.enableSimpleBroker("/topic");
+        // /topic/messages
+        config.setApplicationDestinationPrefixes("/app");
+        // /add/chat
+        // server-side: @MessagingMapping("/chat")
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry)
+    {
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("https:localhost:3000") // change before host
+                .withSockJS();
+    }
+    // Established a connection on chat endpoint.
+}
