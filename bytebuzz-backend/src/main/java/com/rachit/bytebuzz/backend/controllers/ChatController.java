0e5b3ef1c3d7258bf43585c802bbe933e9bd86ea
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-18 02:10:02 +0530

    feat: Add ChatController.java

--- bytebuzz-backend/src/main/java/com/rachit/bytebuzz/backend/controllers/ChatController.java
+++ bytebuzz-backend/src/main/java/com/rachit/bytebuzz/backend/controllers/ChatController.java
@@ -1,4 +1,52 @@
 package com.rachit.bytebuzz.backend.controllers;
 
-public class ChatController {
+
+import com.rachit.bytebuzz.backend.entities.Message;
+import com.rachit.bytebuzz.backend.entities.Room;
+import com.rachit.bytebuzz.backend.payload.MessageRequest;
+import com.rachit.bytebuzz.backend.repositories.RoomRepository;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+
+@Controller
+@CrossOrigin("http://localhost:3000")
+public class ChatController
+{
+    private RoomRepository roomRepository;
+
+    public ChatController(RoomRepository roomRepository)
+    {
+        this.roomRepository = roomRepository;
+    }
+
+    // For sending and receiving messages
+    @MessageMapping("/sendMessage/{roomId}") // app/sendMessage/roomId
+    @SendTo("/topic/room/{roomId}") // Subscribe
+    public Message sendMessage(
+            @DestinationVariable String roomId,
+            @RequestBody MessageRequest request
+    ) {
+        Room room = roomRepository.findByRoomId(request.getRoomId());
+
+        Message message = new Message();
+        message.setContent(request.getContent());
+        message.setSender(request.getSender());
+        message.setTimeStamp(LocalDateTime.now());
+        if (room != null)
+        {
+            room.getMessages().add(message);
+            roomRepository.save(room);
+        }
+        else
+        {
+            throw new RuntimeException("Room not found !");
+        }
+        return message;
+    }
 }
