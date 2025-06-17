ee30e64f4beef4e21848b8f795533c1762246ba8
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-18 04:06:02 +0530

    style: format code in Routes.jsx and main.jsx for consistency

--- bytebuzz-frontend/src/main.jsx
+++ bytebuzz-frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, } from "react-router";
-import AppRoutes from './config/routes.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router";
+import AppRoutes from "./config/routes.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <AppRoutes />
+      <AppRoutes />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
