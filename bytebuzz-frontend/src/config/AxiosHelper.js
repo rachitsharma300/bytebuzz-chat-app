e048ddc57875d9a1b6d39c625be5cbf94f66fdfc
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-19 05:15:14 +0530

    feat: update base URL to 8080 and add content type header for room creation API

--- bytebuzz-frontend/src/config/AxiosHelper.js
+++ bytebuzz-frontend/src/config/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8000";
+export const baseURL = "http://localhost:8080";
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
--- bytebuzz-frontend/src/services/RoomService.js
+++ bytebuzz-frontend/src/services/RoomService.js
@@ -1,6 +1,10 @@
 import { httpClient } from "../config/AxiosHelper";
 
 export const createRoomApi = async (roomDetail) => {
-  const respone = await httpClient.post(`/api/v1/rooms`, roomDetail);
+  const respone = await httpClient.post(`/api/v1/rooms`, roomDetail, {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
   return respone.data;
 };
