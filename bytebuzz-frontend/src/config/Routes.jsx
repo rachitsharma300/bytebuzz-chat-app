fbe94d2a17cf2b6d16b64300f61407655ae58e0f
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-18 06:45:56 +0530

    feat: import ChatPage component in Routes for chat routing

--- bytebuzz-frontend/src/config/Routes.jsx
+++ bytebuzz-frontend/src/config/Routes.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import App from "../App";
+import ChatPage from "../components/ChatPage";
 
 const AppRoutes = () => {
-  return (
+    return (
     <Routes>
       <Route path="/" element={<App />} />
-      <Route
-        path="/test"
-        element={<h1>This is a testing page for Routes</h1>}
-      />
+      <Route path="/test" element={<h1>This is a testing page for Routes</h1>} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
-  );
+    );
 };
 
 export default AppRoutes;
