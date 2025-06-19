956fdab6f03383a3c7568b4e48d3bd7c72ea35cd
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-20 01:30:22 +0530

    Add state management to App component for future functionality

--- bytebuzz-frontend/src/App.jsx
+++ bytebuzz-frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import toast from "react-hot-toast";
 import "./App.css";
 import JoinCreateChat from "./components/JoinCreateChat";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <JoinCreateChat />
