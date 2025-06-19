ee3f8616db1bec3571bd92a8d64cfaae1e903ea5
rachitsharma300 <rachitsharma300@gmail.com>
2025-06-19 06:00:35 +0530

    feat: wrap AppRoutes with ChatProvider for context management

--- bytebuzz-frontend/src/main.jsx
+++ bytebuzz-frontend/src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { BrowserRouter } from "react-router";
 import AppRoutes from "./config/routes.jsx";
 import { Toaster } from "react-hot-toast";
+import { ChatProvider } from "./context/ChatContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
     <Toaster position="top-right" />
-      <AppRoutes />
+      <ChatProvider>
+        <AppRoutes />
+      </ChatProvider>
     </BrowserRouter>
   </StrictMode>
 );
